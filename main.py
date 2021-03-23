82c972713c20e6a5c6f6ed27b1e7508396c788aa
TralExCoDev <tralexcodev@gmail.com>
2021-03-23 13:39:40 +0530

    Pixel Implementation

--- main.py
+++ main.py
@@ -4,18 +4,38 @@ from PIL import Image
 
 pygame.init()
 
+class Pixel:
+    def __init__(self,x,y):
+        self.pixel = pygame.Rect(x, y, block,block)
+    def get_pix(self):
+        return self.pixel
+
+pixels = []
+
+#sorting image into 100,100,3 array
 im = Image.open('image.png')
 na = np.array(im)
 
-res = input("Resolution : ").split(",")
-x = int(res[0])
-y = int(res[1])
-block = 600/x
+#defining resolution
+x_l = len(na[0])
+y_l = len(na)
+block = 600/x_l
+
+#creating window
 surface = pygame.display.set_mode((600,600)) 
 color = (255,0,0) 
 running = True
-pixel = pygame.Rect(30, 30, block,block)
-pygame.draw.rect(surface,color,pixel) 
+
+#going through each pixel of the image and creating a pixel for it using the pixel class
+for x in range(x_l):
+    for y in range(x):
+        pixels.append(Pixel(x* block , y * block).get_pix())
+
+#drawing wach of the pixel in the list of pixels
+for p in pixels:
+    pygame.draw.rect(surface,color,p) 
+
+#main GameLoop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
