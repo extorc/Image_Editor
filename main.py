60ae2edd8f98d2ddecf759324c0a13c9f86a1aa4
TralExCoDev <tralexcodev@gmail.com>
2021-03-24 08:51:39 +0530

    Added File System Support

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import pygame 
 import numpy as np
-from PIL import Image
+import PIL.Image
 from EditorUtils import Pixel as p
 from EditorUtils import *
+from tkinter import *
+from tkinter import filedialog
+
+tk=Tk()
+file = filedialog.askopenfilename()
+tk.destroy()
+im = PIL.Image.open(file)
+na = np.array(im)
+
 pygame.init()
 pixels = []
 
 #sorting image into 100,100,3 array
-im = Image.open('Blackhole.png')
-na = np.array(im)
+
 
 #defining resolution
 x_l = len(na[0])
@@ -16,7 +24,6 @@ y_l = len(na)
 greater = max(x_l, y_l)
 x_res = get_window_resolution(x_l, y_l, greater)
 aspect_ratio = (x_l/y_l)
-print(aspect_ratio)
 block = x_res/x_l
 
 #creating window
